12cd03961e9b71bd3bed4a6ae996c66374266ddb
Samuel <samuelruizmartin2412@gmail.com>
2026-02-01 21:48:49 +0100

    Ejercicio 01

---
+++ Trabajos/RELACION EJERCICIOS 01/EJ básicos/EJ01/app.js
@@ -0,0 +1,13 @@
+"use strict";
+const nombre = "Samuel";
+const edad = 18;
+
+const app = document.querySelector("#app");
+
+const parrafo = document.createElement("p")
+
+parrafo.innerHTML = `Me llamo ${nombre} y tengo ${edad} años`;
+
+app.appendChild(parrafo);
+
+parrafo.className=("EJ01");
